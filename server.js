5db458fa17f0ea99c932002513b64a032d3a7c05
Angelika Mikołajczyk <angelika.mikolajczyk11@gmail.com>
2020-12-02 14:44:34 +0100

    add file analyse endpoint

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,13 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  const name = req.file.originalname;
+  const type = req.file.mimetype;
+  const size = req.file.size;
 
+  res.json({name, type, size: Number(size)});
+})
 
 
 const port = process.env.PORT || 3000;
